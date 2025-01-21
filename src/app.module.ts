684b96d3a02d964689f5c7bdc3725ce9520d9126
Luis Zambrano <lazm.dev@gmail.com>
2025-01-21 18:54:01 -0400

    fix: build and config enhancements

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     BooksModule,
     MongooseModule.forRoot(
-      `mongodb://localhost:27017/fairwind_db?retryWrites=true&w=majority`,
+      process.env.DB_URL,
     ),
   ],
   controllers: [AppController],
